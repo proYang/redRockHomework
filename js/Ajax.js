623b1e499cf148b107e8053feaaba51fa6575308
proYang <904509221@qq.com>
2016-04-20 21:45:54 +0800

    Ajax获取Json数据
    处理时间戳

--- js/Ajax.js
+++ js/Ajax.js
@@ -1,17 +1,25 @@
 
 function getMessage() {
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET","http://api.openweathermap.org/data/2.5/forecast/city?id=1814906&APPID=1111111111");
+	xhr.open("GET","http://openweathermap.org/data/2.5/forecast/daily?id=1814906&appid=b1b15e88fa797225412429c1c50c122a");
 	xhr.send();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
 			if (xhr.status == 200) {
 				var data = JSON.parse(xhr.responseText);
-				console.log(data);
+				showMessage(data);
 			} else {
 				alert("error"+xhr.status);
 			}
 		}
+	}
+}
+function showMessage(data) {
+	document.querySelector("#date").innerHTML = data.city.name;
+	for (var i = 0; i < data.list.length; i++) {
+		var d = new Date(data.list[i].dt * 1000).toLocaleDateString();
+		d = d.split("/");
+		console.log(d[0]+"年"+d[1]+"月"+d[2]+"日");
 	}
 }
 document.querySelector("#getData").addEventListener("click",getMessage);
