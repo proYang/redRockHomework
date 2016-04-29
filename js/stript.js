63973450f4205215e70f6db0408a41207b4c8898
SlaneYang <904509221@qq.com>
2016-04-29 15:13:03 +0800

    完成表单邮箱及密码正则验证

--- js/stript.js
+++ js/stript.js
@@ -1,5 +1,16 @@
 window.onload = function () {
-	var name = document.forms[0].Email.value;
-	var password = document.forms[0].password.value;
-
+	var email = document.forms[0].Email;
+	var password = document.forms[0].password;
+	email.addEventListener("blur",function () {
+		var patt = /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/;
+		if(!patt.test(email.value)) {
+			alert("Email格式错误！");
+		}
+	});
+	password.addEventListener("blur",function () {
+		var patt = /^[\@A-Za-z0-9\!\#\$\%\^\&\*\.\~]{6,22}$/;
+		if(!patt.test(email.value)) {
+			alert("密码格式不符合要求！");
+		}
+	});
 }
